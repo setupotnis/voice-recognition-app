77a6d17c712d27965a3aec24cc24d355c4d5a0b7
Setu Potnis <46113818+setupotnis@users.noreply.github.com>
2019-07-08 11:21:03 -0400

    added styling to button and reply functionality

--- text-to-speech-app/app.js
+++ text-to-speech-app/app.js
@@ -1,10 +1,17 @@
 const btn = document.querySelector('.talk');
 const content = document.querySelector('.content');
 
-
 const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-
 const recognition = new SpeechRecognition()
+
+const motivation = [ 'I\'m doing well, what about you?', 
+                    'feeling pretty down, you should be more productive', 
+                    'have you read any books today?',
+                    'Why don\'t you be productive with your time instead',
+                    'you only have 24 hours in a day, are you happy with the way you used them?',
+                    'you\'re working hard, but you can definitely be more focused',
+                    'are you happy with what you accomplished today?',
+                    'Is that all you\'re going to work on for now?'];
 
 recognition.onstart = function (){
     console.log('voice is activated, you can talk to microphone');
@@ -28,10 +35,16 @@ btn.addEventListener('click', () => {
 
 function readOutloud(message) {
     const speech = new SpeechSynthesisUtterance();
+
+    speech.text = 'please repeat what you said more clearly';
+
+    if(message.includes('what', 'how','you', 'I', 'any', 'today', 'work')){
+        const output = motivation[Math.floor(Math.random() * motivation.length)]
+        speech.text = output;
+    };
+
     speech.volume = 1;
     speech.rate = 1;
     speech.pitch = 1;
-    speech.text = message;
-
     window.speechSynthesis.speak(speech);
 }
